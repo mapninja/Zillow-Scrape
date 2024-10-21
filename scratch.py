1a34d3b73d3abdf10b0f981a29e05c18027aec39
mapninja <stacey.maples@gmail.com>
2024-10-21 07:57:53 -0700

    Create scripts to scrape zillow based on a BBox from a geojson.

---
+++ scratch.py
@@ -0,0 +1,17 @@
+    filters = {
+        "isForSaleForeclosure": {"value": False},
+        "isMultiFamily": {"value": True},
+        "isAllHomes": {"value": False},
+        "isAuction": {"value": False},
+        "isNewConstruction": {"value": False},
+        "isForRent": {"value": False},
+        "isLotLand": {"value": False},
+        "isManufactured": {"value": False},
+        "isForSaleByOwner": {"value": False},
+        "isComingSoon": {"value": False},
+        "isForSaleByAgent": {"value": True},
+        "price":{"max":1000000},
+        "lot":{"min":5000},
+        "beds":{"min":1},
+        "baths":{"min":1}
+        }
